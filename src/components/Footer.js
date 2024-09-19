3fff4d7561ab7444f0919d3172910b3f6e5eac1b
Clifford Anderson <c.andersoninfotech@gmail.com>
2024-09-18 21:47:47 -0500

    pulling jokes from API

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,32 @@
 import React from 'react';
-import cat2 from '../images/cat2.jpeg'
-import cat3 from '../images/cat3.jpeg'
+import cat1 from '../images/cat1.jpeg';
+import cat2 from '../images/cat2.jpeg';
+import cat3 from '../images/cat3.jpeg';
+import cat4 from '../images/cat4.jpeg';
+import cat5 from '../images/cat5.jpg';
+import cat6 from '../images/cat6.jpeg';
+import cat7 from '../images/cat7.jpeg';
+import cat8 from '../images/cat8.jpeg';
 
 export default function Footer() {
+
+  const catsArray = [
+    cat1,
+    cat2,
+    cat3,
+    cat4,
+    cat5,
+    cat6,
+    cat7,
+    cat8
+  ];
+
+  const randomNumber = Math.floor(Math.random() * catsArray.length)
 
   return (
     <footer>
       {/* <div className='cat-pic'></div> */}
-      <img src={cat3} className='cat-pic' alt='cat sitting on couch' />
+      <img src={catsArray[randomNumber]} className='cat-pic' alt='cat sitting on couch' />
       <div className=''>
         {/* <hr className='border-hr'/> */}
       </div>
--- src/components/Joke.js
+++ src/components/Joke.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function Jokes() {
+
+  const [ joke, setJoke ] = useState('');
+  const [ response, setResponse ] = useState('');
+
+  useEffect(() => {
+    async function getJoke() {
+      const resp = await fetch('https://official-joke-api.appspot.com/random_joke');
+      const data = await resp.json();
+      setJoke(data.setup);
+      setResponse(data.punchline);
+    } 
+    getJoke();
+  }, [])
 
   return (
 
@@ -8,13 +21,13 @@ export default function Jokes() {
       <div className='flex'>
         {/* SETUP */}
         <p className='setup'>
-          What do you call a fashionable lawn statue with an excellent sense of rhythmn?
+          {joke}
         </p>
       </div>
       <div>
         {/* PUNCHLINE */}
         <p className='punchline'>
-        😅A metro-gnome🤣
+        😅{response}🤣
         </p>
         {/* <button>Get Joke</button> */}
       </div>
